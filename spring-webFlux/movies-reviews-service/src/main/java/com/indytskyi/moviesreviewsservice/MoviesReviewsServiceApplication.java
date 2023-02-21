75a72fa43a8d5b1092d834b9d15404d3819ff8c8
Indytskyi Artem <ind.artem2003@gmail.com>
2023-02-21 15:32:37 +0200

    configure eureka

--- spring-webFlux/movies-reviews-service/src/main/java/com/indytskyi/moviesreviewsservice/MoviesReviewsServiceApplication.java
+++ spring-webFlux/movies-reviews-service/src/main/java/com/indytskyi/moviesreviewsservice/MoviesReviewsServiceApplication.java
@@ -2,7 +2,9 @@ package com.indytskyi.moviesreviewsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class MoviesReviewsServiceApplication {
 
